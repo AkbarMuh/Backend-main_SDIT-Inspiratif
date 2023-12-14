230ffc71267c873dec6cd4c2904ef169ee14c1e1
AkbarMuh <akbar.muhammt@gmail.com>
2023-12-14 13:22:20 +0700

    update postman dan sql

--- crud_db.sql
+++ crud_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Des 2023 pada 05.41
+-- Waktu pembuatan: 14 Des 2023 pada 07.21
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 8.1.5
 
@@ -35,9 +35,7 @@ CREATE TABLE `guru` (
   `Tempat_Lahir` varchar(50) NOT NULL,
   `Tanggal_Lahir` date NOT NULL,
   `Alamat` varchar(255) NOT NULL,
-  `Lulusan` varchar(255) NOT NULL,
-  `Jurusan` varchar(255) NOT NULL,
-  `Nama_Perguruan_Tinggi` varchar(255) NOT NULL,
+  `Lulusan` text NOT NULL,
   `Sertifikat` text NOT NULL DEFAULT '-',
   `publikasi` text NOT NULL DEFAULT '-',
   `KK` text NOT NULL DEFAULT '-',
@@ -49,19 +47,19 @@ CREATE TABLE `guru` (
 -- Dumping data untuk tabel `guru`
 --
 
-INSERT INTO `guru` (`nama`, `ID`, `Email`, `Nomor_HP`, `Tempat_Lahir`, `Tanggal_Lahir`, `Alamat`, `Lulusan`, `Jurusan`, `Nama_Perguruan_Tinggi`, `Sertifikat`, `publikasi`, `KK`, `createdAt`, `updatedAt`) VALUES
-('admin', '0', '0', '0', '', '0000-00-00', '0', '0', '0', '0', '-', '-', '-', '2023-11-04 04:10:54', '2023-11-04 04:10:54'),
-('Kori', '13', 'kori@gmail.com', '08228161931', 'Jepara', '2012-12-02', 'Jepara', 'S1', 'Informatika', 'Unpad', 'wkwkwkwk', 'lwwlwllw', 'wwkkwkwkw', '2023-12-12 04:14:14', '2023-12-12 04:14:40'),
-('Yui', '13012124', 'yui@email.com', '08123456789', 'Bandung', '1990-01-01', 'Jl. Otto Iskandar Dinata, Karanganyar, Kec. Astanaanyar, Kota Bandung, Jawa Barat 40241', 'S1', 'Pendidikan', 'Unpad', '-', '-', 'KK123456', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
-('Andika', '13012125', 'andika@email.com', '08234567890', 'Bandung', '1985-05-15', 'Jl. Padang Golf, Sukamiskin, Kec. Arcamanik, Kota Bandung, Jawa Barat 40293', 'S1', 'Pendidikan', 'UI', 'Guru Profesional', 'Mengajar Metode Penelitian', 'KK234567', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
-('Taya', '13012132', 'taya@email.com', '08345678901', 'Bandung', '1987-08-20', 'Jl. Merdeka No.45, Merdeka, Kec. Merdeka, Kota Bandung, Jawa Barat 40265', 'S2', 'Psikologi', 'ITB', 'Sertifikasi Guru Profesional', 'Menulis Buku Psikologi Anak', 'KK345678', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
-('Ayub', '13012133', 'ayub@email.com', '08456789012', 'Bandung', '1980-12-10', 'Jl. Cinta No.23, Cinta, Kec. Bahagia, Kota Bandung, Jawa Barat 40287', 'S1', 'Pendidikan', 'Unpad', 'Sertifikat Pendidik', 'Mengajar Bahasa Indonesia', 'KK456789', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
-('Hazel', '13012134', 'hazel@email.com', '08567890123', 'Bandung', '1983-06-25', 'Jl. Indah No.78, Indah, Kec. Bahagia, Kota Bandung, Jawa Barat 40266', 'S3', 'Pendidikan Bahasa Inggris', 'UNJ', '-', '-', 'KK567890', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
-('Ashleigh', '13012135', 'ashleigh@email.com', '08678901234', 'Bandung', '1989-03-18', 'Jl. Megah No.56, Megah, Kec. Megah, Kota Bandung, Jawa Barat 40287', 'S2', 'Manajemen Bisnis', 'Binus', '-', 'Mengelola Seminar Bisnis', 'KK678901', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
-('Mariah', '13012136', 'mariah@email.com', '08789012345', 'Bandung', '1982-07-30', 'Jl. Kaya No.34, Kaya, Kec. Megah, Kota Bandung, Jawa Barat 40266', 'S2', 'Pendidikan', 'UPI', 'Sertifikat Kepemimpinan', 'Menjadi Kepala Sekolah', 'KK789012', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
-('Dewi', '13012137', 'dewi@email.com', '08890123456', 'Bandung', '1986-09-08', 'Jl. Jaya No.12, Jaya, Kec. Bahagia, Kota Bandung, Jawa Barat 40287', 'S1', 'Pertanian', 'IPB', 'Sertifikat Keahlian Pertanian', 'Mengelola Kelas Pertanian', 'KK890123', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
-('Teddy', '13012138', 'teddy@email.com', '08901234567', 'Bandung', '1984-11-22', 'Jl. Makmur No.78, Makmur, Kec. Megah, Kota Bandung, Jawa Barat 40266', 'S1', 'Ilmu Komputer', 'ITB', 'Sertifikat Pendidik', 'Mengajar Pemrograman', 'KK901234', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
-('Margaret', '13012139', 'margaret@email.com', '09012345678', 'Bandung', '1981-04-03', 'Jl. Cerdas No.45, Cerdas, Kec. Megah, Kota Bandung, Jawa Barat 40287', 'S2', 'Pendidikan Matematika', 'UNJ', 'Sertifikat Pelatihan Guru', 'Menulis Buku Pelajaran Matematika', 'KK012345', '2023-10-08 10:06:59', '2023-10-08 10:06:59');
+INSERT INTO `guru` (`nama`, `ID`, `Email`, `Nomor_HP`, `Tempat_Lahir`, `Tanggal_Lahir`, `Alamat`, `Lulusan`, `Sertifikat`, `publikasi`, `KK`, `createdAt`, `updatedAt`) VALUES
+('admin', '0', '0', '0', '', '0000-00-00', '0', '0', '-', '-', '-', '2023-11-04 04:10:54', '2023-11-04 04:10:54'),
+('Kori', '13', 'kori@gmail.com', '08228161931', 'Jepara', '2012-12-02', 'Jepara', 'S1', 'wkwkwkwk', 'lwwlwllw', 'wwkkwkwkw', '2023-12-14 06:18:43', '2023-12-14 06:18:43'),
+('Yui', '13012124', 'yui@email.com', '08123456789', 'Bandung', '1990-01-01', 'Jl. Otto Iskandar Dinata, Karanganyar, Kec. Astanaanyar, Kota Bandung, Jawa Barat 40241', 'S1', '-', '-', 'KK123456', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
+('Andika', '13012125', 'andika@email.com', '08234567890', 'Bandung', '1985-05-15', 'Jl. Padang Golf, Sukamiskin, Kec. Arcamanik, Kota Bandung, Jawa Barat 40293', 'S1', 'Guru Profesional', 'Mengajar Metode Penelitian', 'KK234567', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
+('Taya', '13012132', 'taya@email.com', '08345678901', 'Bandung', '1987-08-20', 'Jl. Merdeka No.45, Merdeka, Kec. Merdeka, Kota Bandung, Jawa Barat 40265', 'S2', 'Sertifikasi Guru Profesional', 'Menulis Buku Psikologi Anak', 'KK345678', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
+('Ayub', '13012133', 'ayub@email.com', '08456789012', 'Bandung', '1980-12-10', 'Jl. Cinta No.23, Cinta, Kec. Bahagia, Kota Bandung, Jawa Barat 40287', 'S1', 'Sertifikat Pendidik', 'Mengajar Bahasa Indonesia', 'KK456789', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
+('Hazel', '13012134', 'hazel@email.com', '08567890123', 'Bandung', '1983-06-25', 'Jl. Indah No.78, Indah, Kec. Bahagia, Kota Bandung, Jawa Barat 40266', 'S3', '-', '-', 'KK567890', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
+('Ashleigh', '13012135', 'ashleigh@email.com', '08678901234', 'Bandung', '1989-03-18', 'Jl. Megah No.56, Megah, Kec. Megah, Kota Bandung, Jawa Barat 40287', 'S2', '-', 'Mengelola Seminar Bisnis', 'KK678901', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
+('Mariah', '13012136', 'mariah@email.com', '08789012345', 'Bandung', '1982-07-30', 'Jl. Kaya No.34, Kaya, Kec. Megah, Kota Bandung, Jawa Barat 40266', 'S2', 'Sertifikat Kepemimpinan', 'Menjadi Kepala Sekolah', 'KK789012', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
+('Dewi', '13012137', 'dewi@email.com', '08890123456', 'Bandung', '1986-09-08', 'Jl. Jaya No.12, Jaya, Kec. Bahagia, Kota Bandung, Jawa Barat 40287', 'S1', 'Sertifikat Keahlian Pertanian', 'Mengelola Kelas Pertanian', 'KK890123', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
+('Teddy', '13012138', 'teddy@email.com', '08901234567', 'Bandung', '1984-11-22', 'Jl. Makmur No.78, Makmur, Kec. Megah, Kota Bandung, Jawa Barat 40266', 'S1', 'Sertifikat Pendidik', 'Mengajar Pemrograman', 'KK901234', '2023-10-08 10:06:59', '2023-10-08 10:06:59'),
+('Margaret', '13012139', 'margaret@email.com', '09012345678', 'Bandung', '1981-04-03', 'Jl. Cerdas No.45, Cerdas, Kec. Megah, Kota Bandung, Jawa Barat 40287', 'S2', 'Sertifikat Pelatihan Guru', 'Menulis Buku Pelajaran Matematika', 'KK012345', '2023-10-08 10:06:59', '2023-10-08 10:06:59');
 
 -- --------------------------------------------------------
 
@@ -87,7 +85,7 @@ CREATE TABLE `kelas` (
 INSERT INTO `kelas` (`ID`, `Grade_Kelas`, `Nama_Angkatan`, `WaliKelas`, `NamaKelas`, `Tahun_Masuk`, `createdAt`, `updatedAt`) VALUES
 (1, 2, 'Alif', '13012135', 'Adam', 2020, '2023-12-07 03:52:31', '2023-12-07 03:52:31'),
 (2, 3, 'Iqro', '13012132', 'Hawa', 2021, '2023-12-07 03:52:31', '2023-12-07 03:52:31'),
-(12, 5, 'Ibnusabil', NULL, 'Umar', 2022, '2023-12-12 04:17:27', '2023-12-12 04:20:31');
+(1231231322, 2, 'RPL', NULL, '1213', 1231, '2023-12-14 06:50:29', '2023-12-14 06:50:29');
 
 -- --------------------------------------------------------
 
@@ -149,8 +147,7 @@ INSERT INTO `ortu` (`ID`, `Jenis`, `nama`, `pekerjaan`, `Tempat_Lahir`, `Tanggal
 (1301219, 'Ibu', 'Teddy Nash', 'Seniman', '-', NULL, 'Jl. Kreatif No.10, Kreator, Kec. Inspirasi, Kota Bandung, Jawa Barat 40265', 1700000, 'D4', 'teddy@email.com', '08901234567', 1302007, '2023-11-04 03:16:42', '2023-11-04 02:19:26'),
 (1301220, 'Ayah', 'Margaret Clarke', 'Insinyur', '-', NULL, 'Jl. Teknologi No.23, Teknologi, Kec. Canggih, Kota Bandung, Jawa Barat 40287', 2300000, 'S2', 'margaret@email.com', '09012345678', 1302007, '2023-11-04 03:20:25', '2023-11-04 03:20:25'),
 (1301222, 'Ayah', 'Bertha Farley', 'Petani', '-', NULL, 'Jl. Terusan Buah Batu No.227, Kujangsari, Kec. Bandung Kidul, Kota Bandung, Jawa Barat 40287', 1300000, 'S1', 'bertha@email.com', '081234567801', 1302008, '2023-11-04 03:20:25', '2023-11-04 03:20:23'),
-(1301223, 'Ibu', 'Inaya Hughes', 'Dosen', '-', NULL, 'Jl. Suryalaya Indah No.1-3, Cijagra, Kec. Lengkong, Kota Bandung, Jawa Barat 40265', 1600000, 'S1', 'inaya@email.com', '081234567802', 1302008, '2023-11-04 03:16:43', '2023-11-04 02:19:27'),
-(12323123, 'Wali', 'Nani', 'Petani', 'Semarang', '2000-11-09', 'Bojong soang', 2000000, 'S1', 'nani@gmail.com', '08227972691', NULL, '2023-12-12 04:09:44', '2023-12-12 04:10:47');
+(1301223, 'Ibu', 'Inaya Hughes', 'Dosen', '-', NULL, 'Jl. Suryalaya Indah No.1-3, Cijagra, Kec. Lengkong, Kota Bandung, Jawa Barat 40265', 1600000, 'S1', 'inaya@email.com', '081234567802', 1302008, '2023-11-04 03:16:43', '2023-11-04 02:19:27');
 
 -- --------------------------------------------------------
 
@@ -206,7 +203,6 @@ CREATE TABLE `siswa` (
 --
 
 INSERT INTO `siswa` (`ID`, `nama`, `NIS`, `NIK`, `No_KartuKeluarga`, `Tempat_lahir`, `Tanggal_Lahir`, `Alamat`, `Tahun_MasukSDIT`, `AsalTK`, `Asal_SD`, `Prestasi`, `LinkPrestasi`, `Wali_Kelas`, `kelas`, `createdAt`, `updatedAt`) VALUES
-(111, 'aku nih 111', '11221', '44445', '91818181', 'bdg', '0001-09-12', 'aaasssssss', '2020', NULL, '-', 'silat', '-', NULL, NULL, '2023-12-12 04:08:30', '2023-12-12 04:09:05'),
 (1302003, 'Budi', '1902003', '2302003', '102302003', 'Bandung', '2007-08-01', 'Jl. Cijawura No.12, Cijawura, Kec. Rancaekek, Kota Bandung, Jawa Barat 40266', '2023', 'TK Bunga Indah', 'SDN Cijawura', 'Juara Lomba Cerdas Cermat', 'linkprestasi.com/budi', '13012124', 1, '2023-11-20 09:30:15', '2023-11-26 10:15:45'),
 (1302004, 'Citra', '1902004', '2302004', '102302004', 'Bandung', '2007-12-23', 'Jl. Melodi Indah No.15, Melodika, Kec. Musikal, Kota Bandung, Jawa Barat 40265', '2023', 'TK Harmoni', 'SDN Melodi', 'Peringkat 2 Lomba Mewarnai', 'linkprestasi.com/citra', '13012124', 2, '2023-11-15 13:45:22', '2023-11-26 10:15:45'),
 (1302005, 'Dian', '1902005', '2302005', '102302005', 'Bandung', '2015-10-23', 'Jl. Harmoni No.8, Harmoni, Kec. Senandung, Kota Bandung, Jawa Barat 40265', '2023', 'TK Melodi Indah', 'SDN Harmoni', 'Mengikuti Olimpiade Matematika', 'linkprestasi.com/dian', '13012125', 1, '2023-11-10 15:20:18', '2023-11-26 10:15:45'),
